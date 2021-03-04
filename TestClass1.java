e807de23606c92214d15d34398352d586efabf80
Kirill <radysh@inbox.ru>
2021-03-04 14:28:10 +0300

    Home Work 7

---
+++ TestClass1.java
@@ -0,0 +1,37 @@
+package HomeWork7;
+
+public class TestClass1 {
+
+    @BeforeSuite
+    void start() {
+        System.out.println("Начало теста");
+
+    }
+
+    @Test(priority = 1)
+    void test1() {
+        System.out.println("test1: приоритет 1");
+    }
+
+    @Test(priority = 0)
+    void test2() {
+        System.out.println("test2: приоритет 0");
+    }
+
+    @Test(priority = 2)
+    void test3() {
+        System.out.println("test3: приоритет 2");
+    }
+
+
+    @Test(priority = 0)
+    void test4() {
+        System.out.println("test4: приоритет 0");
+    }
+
+    @AfterSuite
+    void stop() {
+        System.out.println("Окончание теста");
+    }
+
+}
---
+++ TestingClass.java
@@ -0,0 +1,67 @@
+package HomeWork7;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+
+public class TestingClass {
+
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        start(TestClass1.class);
+        start("HomeWork7.TestClass2");
+    }
+
+    public static void start(Class<?> testClass) throws IllegalAccessException, InstantiationException, InvocationTargetException {
+
+        Object classObject = testClass.newInstance();
+        Map<Integer, Set<Method>> testMethodsMap = new TreeMap();
+        Method beforeSuiteMethod = null;
+        Method afterSuiteMethod = null;
+
+        Method[] methods = testClass.getDeclaredMethods();
+
+        for (Method method : methods) {
+
+            if (method.isAnnotationPresent(Test.class)) {
+                Test testAnnotation = method.getAnnotation(Test.class);
+                Set<Method> methodSet = testMethodsMap.getOrDefault(testAnnotation.priority(), new HashSet<>());
+                methodSet.add(method);
+                testMethodsMap.put(testAnnotation.priority(), methodSet);
+            }
+
+            if (method.isAnnotationPresent(BeforeSuite.class)) {
+                if (beforeSuiteMethod == null) {
+                    beforeSuiteMethod = method;
+                } else {
+                    throw new RuntimeException();
+                }
+            }
+            if (method.isAnnotationPresent(AfterSuite.class)) {
+                if (afterSuiteMethod == null) {
+                    afterSuiteMethod = method;
+                } else {
+                    throw new RuntimeException();
+                }
+            }
+        }
+
+        beforeSuiteMethod.invoke(classObject);
+
+        for (Integer priority : testMethodsMap.keySet()) {
+            Set<Method> methodSet = testMethodsMap.get(priority);
+            for (Method method : methodSet) {
+                method.invoke(classObject);
+            }
+        }
+
+        afterSuiteMethod.invoke(classObject);
+    }
+
+
+    public static void start(String className) throws ClassNotFoundException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Class testClass = Class.forName(className);
+        start(testClass);
+    }
+
+
+}
